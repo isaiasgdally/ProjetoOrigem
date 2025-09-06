cc360634623516671d5590f7e26bb8c3bba0bfd7
Isaías Dally <isaiasgdally@gmail.com>
2025-09-06 12:27:09 -0300

    Desafio do dia 06/09/2025
    Desafio feito na aula do dia 06/09/2025

--- DESAFIO 01/script.js
+++ DESAFIO 01/script.js
@@ -1,45 +1,47 @@
-let diaDaSemana;
-let idade;
+function verificador(diaDaSemana, idade) {
+    let classificacao;
+
+    
+    if (idade < 12) {
+        classificacao = 'Criança';
+    } else if (idade >= 12 && idade <= 18) {
+        classificacao = 'Adolescente';
+    } else if (idade > 18) {
+        classificacao = 'Adulto';
+    }
+
+    
 
 
+    switch (diaDaSemana) {
+        case 'domingo':
+            console.log('Ir ao parque');
+            break;
+        case 'segunda':
+            console.log('Estudar Programação');
+            break;
+        case 'terça':
+            console.log('Praticar esporte');
+            break;
+        case 'quarta':
+            console.log('Ler um livro');
+            break;
+        case 'quinta':
+            console.log('Assistir um fime');
+            break;
+        case 'sexta':
+            console.log('Encontrar amigos');
+            break;
+        case 'sábado':
+            console.log('Fazer uma viagem curta');
+            break;
+        default:
+            console.log('Dia inválido');
+            break;
+    }
 
-
-function verificador(diaDaSemana, idade){
-
-if(idade < 12){
-    console.log('Criança');
-} else if (idade > 12 && idade <= 18){
-    console.log('Adolescente');
-}else if(idade > 18){
-    console.log('Adulto');
+    return classificacao;
 }
 
-switch(diaDaSemana){
- case 'domingo':
-    console.log('Ir ao parque');
-    break;
-    case 'segunda':
-        console.log('Estudar Programação');
-        break;
-    case 'terça':
-        console.log('Praticar esporte');
-        break;
-    case 'quarta':
-        console.log('Ler um livro');
-        break;
-    case 'quinta':
-        console.log('Assistir um fime');
-        break;
-    case 'sexta':
-        console.log('Encontrar amigos');
-        break;
-    case 'sábado':
-        console.log('Fazer uma viagem curta');
-        break;
-    default:
-        console.log('Dia invalido')
-    break;
-}
-}
 
-console.log(verificador('domingo', 12))
+console.log(verificador('domingo', 12));
